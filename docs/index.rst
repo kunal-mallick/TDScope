dcce2b75b95fae89faed0f95c7f9e23b805a8a64
kunal(asus) <kunalmallicks3@gmail.com>
2025-09-10 23:06:02 +0530

    initial commit

---
+++ docs/index.rst
@@ -0,0 +1,24 @@
+.. TDScope documentation master file, created by
+   sphinx-quickstart.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+TDScope documentation!
+==============================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+   getting-started
+   commands
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='TDScope is a smart analytics tool designed to predict Total Dissolved Solids (TDS) in water using parameters like temperature, pH, and more. Powered by machine learning, it transforms raw environmental data into actionable insights for water quality monitoring, sustainability, and public health.',
+    author='Your name (or your organization/company/team)',
+    license='',
+)
