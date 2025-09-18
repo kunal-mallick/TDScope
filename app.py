4501db0133e4f30cc7bd8c76e00ee72662283e4c
Kunal Mallick <117811040+kunal-mallick@users.noreply.github.com>
2025-09-18 17:33:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ import os
 app = Flask(__name__)
 
 # Load models (ensure these are in the working directory)
-MODEL_FILE = r"models/svr_model.pkl"
-SCALER_FILE = r"models/scaler_model.pkl"
-PCA_FILE = r"models/pca_model.pkl"
+MODEL_FILE = r"models\svr_model.pkl"
+SCALER_FILE = r"models\scaler_model.pkl"
+PCA_FILE = r"models\pca_model.pkl"
 
 model = joblib.load(MODEL_FILE)
 scaler = joblib.load(SCALER_FILE)
